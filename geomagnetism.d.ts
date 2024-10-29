c92cf6d7b1f9b09f7fd3195c9a8600e0a252dbe6
Cristiano Costa <cristiano@cristianocosta.me>
2024-10-29 10:04:12 -0300

    chore: fix .d.ts types

---
+++ geomagnetism.d.ts
@@ -0,0 +1,26 @@
+declare module "@cristianob/geomagnetism" {
+  export interface GeomagnetismPoint {
+    x: number;
+    y: number;
+    z: number;
+    h: number;
+    f: number;
+    incl: number;
+    decl: number;
+  }
+  export interface GeomagnetismModel {
+    epoch: number;
+    start_date: Date;
+    end_date: Date;
+    name: string;
+    main_field_coeff_g: number[];
+    secular_var_coeff_h: number[];
+    n_max: number;
+    n_max_sec_var: number;
+    num_terms: number;
+    point(geoPoint: number[]): GeomagnetismPoint;
+  }
+
+  export function model(date: Date): GeomagnetismModel;
+  export function model(): GeomagnetismModel;
+}
